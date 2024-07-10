749a9c0b55a7572a62c8ba8001f51ab91f0cc6b5
Brian Casel <hi@briancasel.com>
2024-07-10 16:22:59 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,65 +1,65 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./resources/**/*.antlers.html",
-        "./resources/**/*.antlers.php",
-        "./resources/**/*.blade.php",
-        "./resources/**/*.vue",
-        "./content/**/*.md",
-    ],
+  content: [
+    "./resources/**/*.antlers.html",
+    "./resources/**/*.antlers.php",
+    "./resources/**/*.blade.php",
+    "./resources/**/*.vue",
+    "./content/**/*.md",
+  ],
 
-    theme: {
-        extend: {
-            screens: {
-                xs: "400px",
-            },
-            fontFamily: {
-                text: ["barlow", "sans-serif"],
-            },
-            colors: {
-                black: "#182125",
-                white: "#ffffff",
-                gray: {
-                    50: "#f4f5f7",
-                    100: "#e4e8e9",
-                    200: "#cbd3d6",
-                    300: "#a7b3b9",
-                    400: "#7c8c94",
-                    500: "#5c6b73",
-                    600: "#535f67",
-                    700: "#475057",
-                    800: "#3f464b",
-                    900: "#383d41",
-                    950: "#22262a",
-                },
-                primary: {
-                    50: "#fef2f3",
-                    100: "#fee2e4",
-                    200: "#ffc9cd",
-                    300: "#fda4aa",
-                    400: "#fa6f78",
-                    500: "#f1424e",
-                    600: "#df2935",
-                    700: "#bb1a25",
-                    800: "#9b1922",
-                    900: "#801c23",
-                    950: "#46090d",
-                },
-                secondary: {
-                    50: "#f4f7f9",
-                    100: "#eaf0f5",
-                    200: "#d9e3ec",
-                    300: "#c1d1e0",
-                    400: "#a8b9d1",
-                    500: "#91a2c3",
-                    600: "#7180ac",
-                    700: "#68759a",
-                    800: "#555f7e",
-                    900: "#495166",
-                    950: "#2b2f3b",
-                }
-            },
+  theme: {
+    extend: {
+      screens: {
+        xs: "400px",
+      },
+      fontFamily: {
+        text: ["barlow", "sans-serif"],
+      },
+      colors: {
+        black: "#171921",
+        white: "#ffffff",
+        gray: {
+          50: "#f4f5f7",
+          100: "#e4e8e9",
+          200: "#cbd3d6",
+          300: "#a7b3b9",
+          400: "#7c8c94",
+          500: "#5c6b73",
+          600: "#535f67",
+          700: "#475057",
+          800: "#3f464b",
+          900: "#383d41",
+          950: "#22262a",
         },
+        primary: {
+          '50': '#fbffe5',
+          '100': '#f4ffc7',
+          '200': '#e7ff95',
+          '300': '#d2ff57',
+          '400': '#c3f73a',
+          '500': '#9ddd05',
+          '600': '#79b100',
+          '700': '#5b8605',
+          '800': '#4a690b',
+          '900': '#3e590e',
+          '950': '#1f3201',
+        },
+        secondary: {
+          '50': '#f6f7f9',
+          '100': '#eceef2',
+          '200': '#d4d8e3',
+          '300': '#aeb6cb',
+          '400': '#838fad',
+          '500': '#637194',
+          '600': '#4f5b7a',
+          '700': '#404964',
+          '800': '#383f54',
+          '900': '#323748',
+          '950': '#171921',
+        }
+      },
     },
-    plugins: [require("@tailwindcss/typography")],
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
